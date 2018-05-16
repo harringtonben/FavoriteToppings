ac6199768d7705c1108dfc77808335f43aa8c0f3
ben harrington <harringtonben@icloud.com>
2018-05-15 20:39:04 -0500

    working through logic of comparing top 20 pizza lists

--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
 
 namespace ConsoleApp1
@@ -11,7 +12,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var readFile = new FileReader().ReadFile();
-            
+
+            var findTop20 = new TallyList().FindTop20(readFile);
         }
     }
 
@@ -22,6 +24,41 @@ namespace ConsoleApp1
             var fileToRead = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"./pizzas.json"));
 
             return fileToRead;
+        }
+    }
+    
+    public class TallyList
+    {
+        public List<Pizza> FindTop20(List<Pizza> readFile)
+        {
+            var singleIngredientPizza = findSingleIngredientPies(readFile);
+            var twoIngredientPizza = findTwoIngredientPies(readFile);
+
+            return readFile;
+        }
+
+        public List<Pizza> findSingleIngredientPies(List<Pizza> pizzas)
+        {
+            var singleIngPies = new List<Pizza>();
+            foreach (var pizza in pizzas)
+            {
+                if(pizza.Toppings.Count == 1)
+                    singleIngPies.Add(pizza);
+            }
+
+            return singleIngPies;
+        }
+        
+        public List<Pizza> findTwoIngredientPies(List<Pizza> pizzas)
+        {
+            var doubleIngPies = new List<Pizza>();
+            foreach (var pizza in pizzas)
+            {
+                if(pizza.Toppings.Count == 2)
+                    doubleIngPies.Add(pizza);
+            }
+
+            return doubleIngPies;
         }
     }
 
