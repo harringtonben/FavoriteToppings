7dc94c98e8baa6862d5e56acb5f0a23a26195519
ben harrington <harringtonben@icloud.com>
2018-05-15 19:37:51 -0500

    blew up dotnet core so back to the drawing board

--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace ConsoleApp1
 {
@@ -6,7 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var readFile = new FileReader().ReadFile();
+            Console.WriteLine(readFile);
+        }
+    }
+
+    public class FileReader
+    {
+        public List<List<string>> ReadFile()
+        {
+            using (var file = File.OpenText("pizzas.json"))
+            {
+                var serializer = new JsonSerializer();
+                var pizzas = serializer.Deserialize(file, typeof(List<List<string>>));
+                return (List<List<string>>) pizzas;
+            }
         }
     }
 }
