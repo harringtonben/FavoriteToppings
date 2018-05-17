8a824e89c0625667bf4f4413373d641b11a960a3
ben harrington <harringtonben@icloud.com>
2018-05-17 18:32:35 -0500

    have pizzas all returning by max order number, need to get top 20 now

--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
+using MoreLinq;
 using Newtonsoft.Json;
 
 namespace ConsoleApp1
@@ -30,59 +32,87 @@ namespace ConsoleApp1
     
     public class TallyList
     {
-        public List<Pizza> FindTop20(List<Pizza> readFile)
+        public bool FindTop20(List<Pizza> readFile)
         {
             var convertToppings = ConvertToppingsLists(readFile);
             var orderList = OrderStrings(convertToppings);
-            var topStrings = Top20Pizzas(orderList);
-
-            return readFile;
-        }
-
-        public bool Top20Pizzas(List<string> orderList)
-        {
-
-            var top20Strings = orderList.GroupBy(x => x)
-                .Select(g => new {Value = g.Key, Count = g.Count()})
-                .OrderByDescending(x => x.Count);
-
-            foreach (var item in top20Strings)
-            {
-                Console.WriteLine($"{item.Value} {item.Count}");
-            }
+            var pizzas = Top20Pizzas(orderList);
+            var top20Pizzas = FilterPizzas(pizzas);
             
             return true;
         }
 
-        private List<string> OrderStrings(List<string> convertToppings)
+        private List<Pizza> FilterPizzas(List<Pizza> pizzas)
         {
-            var twistedLetters = new List<string>();
-            foreach (var topping in convertToppings)
-            {
-                var alphabetizedString = topping.OrderBy(x => x).ToArray();
-                var orderedString = String.Join("", alphabetizedString);
-                twistedLetters.Add(orderedString);
-            }
+            var distinctPizzas = pizzas.DistinctBy(x=> x.NumberOfOrders).ToList();
+            var orderedPizzas = distinctPizzas.MaxBy(x => x.NumberOfOrders).ToList();
+            
+            Console.WriteLine(pizzas.Count);
+            Console.WriteLine(distinctPizzas.Count);
 
-            return twistedLetters;
+            return distinctPizzas;
         }
 
-        private List<string> ConvertToppingsLists(List<Pizza> readFile)
+        public List<Pizza> Top20Pizzas(List<Pizza> orderList)
         {
-            var stringToppings = new List<string>();
+            var allTheZas = new List<Pizza>();
+            
+            var top20Strings = orderList.GroupBy(x => x.ToppingsString)
+                .Select(g => new {Value = g.Key, Count = g.Count()})
+                .OrderByDescending(x => x.Count);
+
+            var top20Pizzas = from order in orderList
+                join item in top20Strings on order.ToppingsString equals item.Value
+                where (order.ToppingsString == item.Value)
+                select new {Toppings = order.Toppings, NumberOfOrders = item.Count, PizzaName = order.PrettToppingString};
+
+            foreach (var item in top20Pizzas)
+            {
+                var za = new Pizza
+                {
+                    Toppings = item.Toppings,
+                    PrettToppingString = item.PizzaName,
+                    NumberOfOrders = item.NumberOfOrders
+                };
+                allTheZas.Add(za);
+            }
+
+            return allTheZas;
+        }
+
+        private List<Pizza> OrderStrings(List<Pizza> convertToppings)
+        {
+
+            foreach (var topping in convertToppings)
+            {
+                var alphabetizedString = topping.ToppingsString.OrderBy(x => x).ToArray();
+                var orderedString = String.Join("", alphabetizedString);
+                topping.ToppingsString = orderedString;
+            }
+
+            return convertToppings;
+        }
+
+        private List<Pizza> ConvertToppingsLists(List<Pizza> readFile)
+        {
+            
             foreach (var file in readFile)
             {
                 var stringList = String.Join("", file.Toppings);
+                file.PrettToppingString = stringList;
                 var removeSpace = Regex.Replace(stringList, @"\s+", "");
-                stringToppings.Add(removeSpace);
+                file.ToppingsString = removeSpace;
             }
 
-            return stringToppings;
+            return readFile;
         }
     }
 
     public class Pizza
     {
         public List<string> Toppings { get; set; }
+        public string PrettToppingString { get; set; }
+        public string ToppingsString { get; set; }
+        public int NumberOfOrders { get; set; }
     }
 }
